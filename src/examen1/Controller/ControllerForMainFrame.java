1f71f5c0c5e881d8e114fac67532570ea422d4bd
Mario <Mario@DESKTOP-39363E7>
2018-02-12 13:41:36 -0600

    Login listo

---
+++ src/examen1/Controller/ControllerForMainFrame.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen1.Controller;
+
+import examen1.View.MainFrame;
+import examen1.Model.ChatBox;
+
+/**
+ *
+ * @author Mario
+ */
+public class ControllerForMainFrame {
+
+    MainFrame mainFrame;
+    ChatBox chatbox;
+
+    public ControllerForMainFrame() {
+
+    }
+
+    public void StartMainFrame() {
+
+        MainFrame mainFrame = new MainFrame();
+    }
+
+}
